d44b583f703deba355a6418beb74f146734e6a81
saroj@agramcloud.com <saroj@agramcloud.com>
2021-04-30 14:51:00 +0545

    project recreated

--- src/main/java/com/agraminfotech/dairymanagement/DairyManagementServiceApplication.java
+++ src/main/java/com/agraminfotech/dairymanagement/DairyManagementServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DairymanagementApplication {
+public class DairyManagementServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DairymanagementApplication.class, args);
+		SpringApplication.run(DairyManagementServiceApplication.class, args);
 	}
 
 }
--- src/test/java/com/agraminfotech/dairymanagement/DairyManagementServiceApplicationTests.java
+++ src/test/java/com/agraminfotech/dairymanagement/DairyManagementServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DairymanagementApplicationTests {
+class DairyManagementServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
